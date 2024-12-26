c70914ed63e5437311250bdaa7b032aedb7917f7
Víctor Alfonso Mejía Morales <130195315+vamm99@users.noreply.github.com>
2024-12-25 21:50:57 -0500

    feat: AgregarAgrega módulo de configuración de NestJS y ajustar el arranque de la aplicación

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoggerConfiguredModule } from './lib/Logger';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [LoggerConfiguredModule],
+  imports: [ConfigModule.forRoot({isGlobal:true,}),LoggerConfiguredModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from 'nestjs-pino';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const configService = app.get(ConfigService);
+  const port = Number(configService.get('PORT'));
 
+  app.enableCors();
+  app.setGlobalPrefix('api');
   app.useLogger(app.get(Logger));
+  await app.listen(port);
 }
 bootstrap();
